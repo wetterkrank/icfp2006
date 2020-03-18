c05553ba299268125f205363c63a96159102b533
Alex Antsiferov <wetterkrank@gmail.com>
2020-03-18 23:33:35 +0100

    moved from a plain array to a struct

--- um32.c
+++ um32.c
@@ -1,23 +1,28 @@
 // A 32-bit UM version using the standard memory allocation routines.
-// Runs sandmark.umz in less than 1 minute. 
+// Runs sandmark.umz in less than 1 minute.
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
+// a pointer-sized integer and its short name
 typedef uintptr_t uint;
 
+struct array {
+    uint length;
+    uint elements[0]; // not the same as *elements!
+};
+
+// endianness converter
 uint myByteSwap(uint num)
 {
     uint b0,b1,b2,b3;
-    uint res = 0;
     b0 = (num & 0x000000ff) << 24u;
     b1 = (num & 0x0000ff00) << 8u;
     b2 = (num & 0x00ff0000) >> 8u;
     b3 = (num & 0xff000000) >> 24u;
-    res = b0 | b1 | b2 | b3;
-    return res;
+    return (b0 | b1 | b2 | b3);
 }
 
 void die(const char *message)
@@ -33,38 +38,41 @@ int main (int args, const char *av[])
     if (args != 2) die("usage: um.exe <program.um>");
 
     // get UM file size; still no error handling for file ops
-    FILE *fp = fopen(av[1], "rb");
-    fseek(fp, 0L, SEEK_END);
-    uint fileSize = ftell(fp);
-    uint progLen = fileSize/4; // todo: check if it's the factor of size(uint)
-    rewind(fp);
+    FILE *file = fopen(av[1], "rb");
+    fseek(file, 0L, SEEK_END);
+    uint fileSize = ftell(file);
+    if (fileSize % 4 != 0) die("invalid file size");
+    uint progLen = fileSize/4;
+    rewind(file);
 
-    // init controls
-    uint *zero;
-    zero = malloc(fileSize+sizeof(uint)); 
+    // read UM program into the "0" array and convert to big endian
+    struct array *zero;
+    zero = malloc(sizeof(*zero)+progLen*sizeof(uint));
     if (zero == NULL) die("program array memory allocation failed");
-    fread(zero+1, sizeof(uint), progLen, fp);
-    fclose(fp);
-    for (uint i = 1; i <= progLen; i++) zero[i] = myByteSwap(zero[i]); //note: <= progLen
-    zero[0] = progLen; //todo: move to struct
+    fread(zero->elements, sizeof(uint), progLen, file);
+    fclose(file);
+    for (uint i = 0; i < progLen; i++)
+        zero->elements[i] = myByteSwap(zero->elements[i]);
+    zero->length = progLen;
 
     uint regs[8] = {0};
-    uint pos = 1; // skipping 0 cell used to keep the program size
+    uint pos = 0;
 
     char operator, A, B, C;
     uint instruction;
 
-    for (; pos <= progLen; pos++)
+    for (; pos < progLen; pos++)
     {
-        instruction = zero[pos];
+        instruction = zero->elements[pos];
         operator = (instruction >> 28) & 15;
         C = instruction & 7;
         B = (instruction >> 3) & 7;
         A = (instruction >> 6) & 7;
-        uint *arr; //tmp
 
-        //if (operator == 13) printf("%d. op: %d, Val %d -> reg %d\n", pos-1, operator, instruction & 0x01FFFFFF, A);
-        //else printf("%d. op: %d, reg %d: %d, reg %d: %d, reg %d: %d\n", pos-1, operator, A, regs[A], B, regs[B], C, regs[C]);
+        struct array *arr; // temporary pointer
+
+        //if (operator == 13) printf("%d. op: %d, Val %d -> reg %d\n", pos, operator, instruction & 0x01FFFFFF, A);
+        //else printf("%d. op: %d, reg %d: %d, reg %d: %d, reg %d: %d\n", pos, operator, A, regs[A], B, regs[B], C, regs[C]);
 
         switch (operator) 
         {
@@ -72,12 +80,12 @@ int main (int args, const char *av[])
                 if (regs[C] != 0) regs[A] = regs[B];
                 break;
             case 1:
-                if (regs[B] != 0) arr = (uint *)regs[B]; else arr = zero;
-                regs[A] = arr[regs[C]+1];
+                if (regs[B] != 0) arr = (struct array *)regs[B]; else arr = zero;
+                regs[A] = arr->elements[regs[C]];
                 break;
             case 2:
-                if (regs[A] != 0) arr = (uint *)regs[A]; else arr = zero;
-                arr[(regs[B]+1)] = regs[C];
+                if (regs[A] != 0) arr = (struct array *)regs[A]; else arr = zero;
+                arr->elements[regs[B]] = regs[C];
                 break;
             case 3:
                 regs[A] = regs[B] + regs[C];
@@ -92,16 +100,16 @@ int main (int args, const char *av[])
                 regs[A] = ~(regs[B] & regs[C]);
                 break;
             case 7:
-                printf("machine halted\n");
                 exit(0);
             case 8: {
-                arr = calloc(regs[C]+1, sizeof(uint));
+                arr = malloc(sizeof(*arr)+regs[C]*sizeof(uint));
                 if (arr == NULL) die("new array memory allocation failed");
-                arr[0] = regs[C];
+                memset(arr, 0, sizeof(*arr)+regs[C]*sizeof(uint));
+                arr->length = regs[C];
                 regs[B] = (uint)arr;
                 break; }
             case 9:
-                free((uint *)regs[C]);
+                free((struct array*)regs[C]);
                 break;
             case 10:
                 printf("%c", regs[C]);
@@ -114,18 +122,19 @@ int main (int args, const char *av[])
             case 12:
                 if (regs[B] != 0) {
                     free(zero);
-                    arr = (uint *)regs[B];
-                    progLen = arr[0];
-                    zero = malloc((progLen+1)*sizeof(uint));
+                    arr = (struct array *)regs[B];
+                    progLen = arr->length;
+                    zero = malloc(sizeof(*arr)+progLen*sizeof(uint));
                     if (zero == NULL) die("program array memory allocation failed");
-                    memcpy(zero, arr, (progLen+1)*sizeof(uint)); }
-                pos = regs[C]; // pos will ++ at the next loop
+                    memcpy(zero, arr, sizeof(*arr)+progLen*sizeof(uint)); }
+                pos = regs[C]-1; // pos will ++ at the next loop
                 break;
             case 13:
-                regs[(instruction >> 25) & 7] = instruction & 0x01FFFFFF; // bits 26-28 and 1-25
+                regs[(instruction >> 25) & 7] = instruction & 0x01FFFFFF;
+                break;
+            default:
+                die("unknown instruction; machine halted");
         };
     };
-
-    printf("end of program\n");
     return 0;
 };
